d904d466073aa48f3cf98e6ef05c1e932f9b5a58
Travis Brewer <t.brewer.co@gmail.com>
2021-07-06 10:24:50 -0600

    wip: establish database connection with connection events, working

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 // dependencies
 require('dotenv').config();
-const { PORT = 4000 } = process.env;
+const { PORT = 4000, MONGODB_URL } = process.env;
 const express = require('express');
+const Mongoose = require('mongoose');
 const app = express();
 
+// establish database connection
+Mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+
+// connection events
+Mongoose.connection
+    .on("open", () => console.log("connected to mongoose")) 
+    .on("close", () => console.log("disconnected from mongoose")) 
+    .on("error", (error) => console.log(error));
+    
+    
 // routes
 app.get('/', (req, res) => {
     res.send('hello, world');
