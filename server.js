a78cfd9d297fb274e1450a8b9e787cd99bff1a7b
Travis Brewer <t.brewer.co@gmail.com>
2021-07-09 05:14:49 -0600

    wip: backend working locally

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 // .env
 //////
 require("dotenv").config();
-const { PORT = 4000, MONGODB_URL } = process.env;
-
+const PORT = process.env.PORT || 4000;
+const MONGODB_URL = process.env.MONGODB_URL;
 //////
 // dependencies/imports
 //////
@@ -12,6 +12,7 @@ const app = express();
 const Mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
+// const methodOVerride = require("method-override");
 
 //////
 // establish database connection
@@ -43,6 +44,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// app.use(methodOverride("_method"));
 
 //////
 // routes
