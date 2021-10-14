29cd131f32cb45d2cd53c7982b6f07edd555b8c3
Travis Brewer <t.brewer.co@gmail.com>
2021-10-13 18:49:08 -0600

    wip: adds name to user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,15 +1,41 @@
-//////
 // dependencies/imports
-//////
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// schemas
 const userSchema = new Schema({
     username: {
         type: String,
         required: true,
-        unique: true,
+        unique: [true, "Username is already taken"],
     },
+    name: String,
+    email: String,
+    password: String,
+    profile_photo: {
+        type: Schema.Types.ObjectId,
+        ref: "Image"
+    },
+    favorite_gyms: [{
+        type: Schema.Types.ObjectId,
+        ref: "Gym"
+    }],
+    favorite_routes: [{
+        type: Schema.Types.ObjectId,
+        ref: "Route"
+    }],
+    created_routes: [{
+        type: Schema.Types.ObjectId,
+        ref: "Route"
+    }],
+    created_gyms: [{
+        type: Schema.Types.ObjectId,
+        ref: "Gym"
+    }],
+    ticks: [{
+        type: Schema.Types.ObjectId,
+        ref: "Tick"
+    }],
 }, {
     timestamps: true,
 });
